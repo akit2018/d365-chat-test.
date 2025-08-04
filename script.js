9d67074e07640b86fb03fa8e4504fe6f35cc3c17
akit2018 <amila@getai-connect.com>
2025-08-04 10:01:33 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // You can add custom JavaScript here if you need to interact with the bot or handle events
+<script id="Microsoft_Omnichannel_LCWidget" 
+src="https://oc-cdn-public-oce.azureedge.net/livechatwidget/scripts/LiveChatBootstrapper.js"" 
+onerror="(function(el){el.parentNode.removeChild(el);var s=document.createElement('script');s.src='https://ocprodpublicocegs.blob.core.windows.net/livechatwidget/scripts/LiveChatBootstrapper.js';s.setAttribute('id', 'Microsoft_Omnichannel_LCWidget');s.setAttribute('data-app-id', '2adf010d-f0b4-49b6-b4e9-74325f6261b1');s.setAttribute('data-lcw-version', 'prod');s.setAttribute('data-org-id', 'e939f707-c636-f011-be50-002248e163c4');s.setAttribute('data-org-url', 'https://m-e939f707-c636-f011-be50-002248e163c4.au.omnichannelengagementhub.com');document.body.appendChild(s);})(this);" 
+data-app-id="2adf010d-f0b4-49b6-b4e9-74325f6261b1" 
+data-lcw-version="prod" 
+data-org-id="e939f707-c636-f011-be50-002248e163c4" 
+data-org-url="https://m-e939f707-c636-f011-be50-002248e163c4.au.omnichannelengagementhub.com" async></script>
+console.log('Copilot Studio Chat Bot Test loaded.');
 console.log('Copilot Studio Chat Bot Test loaded.');
